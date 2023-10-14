7e73c46e18e854fb002b38cfa7958ce464212140
Daniel Rodrigues <up202006562@up.pt>
2023-10-14 18:58:26 +0100

    working on cameras

--- tp1/MyApp.js
+++ tp1/MyApp.js
@@ -71,7 +71,7 @@ class MyApp {
 
         // Create a basic perspective camera
         const perspective1 = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000)
-        perspective1.position.set(10, 10, 3)
+        perspective1.position.set(-40, 20, 40)
         this.cameras['Perspective'] = perspective1
 
         const perspectiveCorner = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000)
@@ -84,8 +84,7 @@ class MyApp {
         this.cameras['Table Perspective'] = perspectiveTable
 
         const perspectiveSofa = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000)
-        perspectiveSofa.position.set(0, 13, 7)
-        perspectiveSofa.lookAt(new THREE.Vector3(-25, 0, 0));
+        perspectiveSofa.position.set(0, 20, -30)
         this.cameras['Sofa Perspective'] = perspectiveSofa
     }
 
