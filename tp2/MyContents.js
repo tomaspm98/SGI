a4f21f298f1dd44ea52870c136fa2dca023c6426
tomaspm98 <up202006845@up.pt>
2023-11-03 21:37:51 +0000

    Scene

--- tp2/MyContents.js
+++ tp2/MyContents.js
@@ -24,7 +24,7 @@ class MyContents {
         this.cameras_map = new Map()
 
         this.reader = new MyFileReader(app, this, this.onSceneLoaded);
-        this.reader.open("scenes/demo/demo.xml");
+        this.reader.open("scenes/demo/MyScene.xml");
 
     }
 
