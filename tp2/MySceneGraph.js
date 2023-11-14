676dbb3f49304a9b53d9d9c9396bfe0414aba56e
Daniel Rodrigues <up202006562@up.pt>
2023-11-14 11:14:33 +0000

    materials working

--- tp2/MySceneGraph.js
+++ tp2/MySceneGraph.js
@@ -12,8 +12,8 @@ class MySceneGraph {
     }
 
     constructSceneGraph() {
-        const root = this.nodes[this.root_id];
-        this.graph = this.constructMeshGraph(root)
+        this.graph = this.constructMeshGraph()
+        this.updateInheritAttributesGraph()
     }
 
     dfs(node, visited = [], materialId = undefined, castShadow = false, receiveShadow = false) {
@@ -65,33 +65,91 @@ class MySceneGraph {
         return sceneNode;
     }
 
-    constructMeshGraph(root) {
+    constructMeshGraph() {
         const meshGraph = new THREE.Group()
         const stack = new Stack()
-        stack.push({node: root, parent: meshGraph})
+        stack.push({node: this.nodes[this.root_id], parent: meshGraph})
+        const visited = []
+
         while (!stack.isEmpty()) {
             const nodeStack = stack.pop()
             const node = nodeStack.node
             const parent = nodeStack.parent
 
-            if (node.type === "spotlight" || node.type === "pointlight" || node.type === "directionallight") {
+            if (visited.hasOwnProperty(node.id)) {
+                const objCloned = visited[node.id].clone()
+                objCloned["isCloned"] = true
+                parent.add(objCloned)
+            } else if (node.type === "spotlight" || node.type === "pointlight" || node.type === "directionallight") {
                 if (node.enabled) {
                     const light = Utils.createThreeLight(node)
                     light.name = node.id
                     parent.add(light)
+                    visited[node.id] = light
                 }
             } else if (node.type === 'primitive') {
                 parent.add(new THREE.Mesh(Utils.createThreeGeometry(node)))
             } else {
                 const newSceneNode = new THREE.Group()
+                newSceneNode.name = node.id
                 Utils.applyTransformation(newSceneNode, node.transformations)
                 parent.add(newSceneNode)
                 for (let i = node.children.length - 1; i >= 0; i--) {
                     stack.push({node: node.children[i], parent: newSceneNode});
                 }
+                visited[node.id] = newSceneNode
             }
         }
-        return meshGraph
+
+        return meshGraph.children.length === 1 ? meshGraph.children[0] : meshGraph
+    }
+
+    updateInheritAttributesGraph() {
+        const stack = new Stack()
+        const rootNode = this.nodes[this.root_id]
+        stack.push({
+            node: rootNode,
+            sceneNode: this.graph,
+            castShadow: rootNode.castShadows,
+            receiveShadow: rootNode.receiveShadows,
+            material: this.materials[rootNode.materialIds[0]]
+        })
+
+        while (!stack.isEmpty()) {
+            const element = stack.pop()
+            
+            const node = element.node
+            const sceneNode = element.sceneNode
+
+            const castShadow = element.castShadow || node.castShadows
+            const receiveShadow = element.receiveShadow || node.receiveShadows
+
+            let material = element.material
+            
+            console.log(node)
+            if (node.materialIds !== undefined && node.materialIds.length > 0) {
+                console.log("TESTE")
+                material = this.materials[node.materialIds[0]]
+            }
+            
+            if (material !== undefined) {
+                sceneNode.material = material
+            }
+            sceneNode.castShadow = castShadow
+            sceneNode.receiveShadow = receiveShadow
+            
+            if(node.children === undefined) continue
+            
+            for (let i = node.children.length - 1; i >= 0; i--) {
+                stack.push({
+                    node: node.children[i],
+                    sceneNode: sceneNode.children[i],
+                    castShadow: castShadow,
+                    receiveShadow: receiveShadow,
+                    material: material
+                });
+            }
+        }
     }
 
     printDfsNode(node_id, visited, castShadow, receiveShadow) {
