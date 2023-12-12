02c646c4b0aa155abe14618f90d5e912dc251bd5
tomaspm98 <up202006845@up.pt>
2023-12-12 12:04:13 +0000

    Car moving with model

--- tp3/vehicle/MyVehicle.js
+++ tp3/vehicle/MyVehicle.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 
 class MyVehicle {
     constructor() {
@@ -9,15 +10,35 @@ class MyVehicle {
         this.rotationchange=0;
         this.maxSpeed=1;
         this.minSpeed=-0.5;
+        this.isLoaded=false;
     }
 
-    build(carMaterial, rectWidth, rectHeight) {
-        const rectangle = new THREE.PlaneGeometry(rectWidth, rectHeight);
+    build(scene) {
+        /*const rectangle = new THREE.PlaneGeometry(rectWidth, rectHeight);
         this.carMesh = new THREE.Mesh(rectangle, carMaterial);
         this.carMesh.rotation.x = Math.PI / 2;
 
-        return this.carMesh;
-    }
+        return this.carMesh;*/
+
+        return new Promise((resolve, reject) => {
+        const loader = new GLTFLoader();
+
+        loader.load('vehicle/scene.gltf',  (gltf) => {
+            this.carMesh = gltf.scene;
+            console.log("car",this.carMesh);
+            this.carMesh.scale.set(0.01,0.01,0.01);
+            this.carMesh.rotation.y=-Math.PI/2;
+            scene.add(this.carMesh);
+            resolve(this.carMesh);
+            this.isLoaded=true;
+        }, undefined, function (error) {
+            console.log("error");
+            console.error("eror",error);
+            reject(error);
+        }
+     );
+    });
+}
 
     controlCar() {
         document.addEventListener('keydown', (event) => {
@@ -27,13 +48,13 @@ class MyVehicle {
                     this.speedchange = 0.01; // Increase speed forward
                     break;
                 case 83: // S key
-                    this.speedchange = -0.01; // Increase speed backward
+                    this.speedchange = -0.005; // Increase speed backward
                     break;
                 case 68: // A key
-                    this.rotationchange = 0.05; // Increase rotation speed left
+                    this.rotationchange = -0.05; // Increase rotation speed left
                     break;
                 case 65: // D key
-                    this.rotationchange = -0.05; // Increase rotation speed right
+                    this.rotationchange = 0.05; // Increase rotation speed right
                     break;
             }
         });
@@ -48,6 +69,7 @@ class MyVehicle {
     
 
     update() {
+        if(this.isLoaded){
             this.controlCar();
             if (this.speed < this.maxSpeed && this.speed> this.minSpeed) {
                 this.speed += this.speedchange;
@@ -60,16 +82,21 @@ class MyVehicle {
 
             }
 
+            
+
             this.rotationspeed += this.rotationchange;
     
-            this.carMesh.position.x += this.speed * Math.cos(this.carMesh.rotation.z);
-            this.carMesh.position.z += this.speed * Math.sin(this.carMesh.rotation.z);
+            this.carMesh.position.z -= this.speed * Math.cos(this.carMesh.rotation.y);
+            this.carMesh.position.x -= this.speed * Math.sin(this.carMesh.rotation.y);
+
+            console.log(this.carMesh.rotation.y)
     
-            this.carMesh.rotation.z += this.rotationspeed;
+            this.carMesh.rotation.y += this.rotationspeed;
     
             this.rotationspeed = 0;
-            console.log("SPEED",this.speed)
+
     }
+}
 }
 
 export { MyVehicle };
