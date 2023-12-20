259594227df4c100f5e85d1956faf408ba859483
Daniel Rodrigues <up202006562@up.pt>
2023-12-20 21:11:29 +0000

    .

--- tp3/vehicle/MyVehicle.js
+++ tp3/vehicle/MyVehicle.js
@@ -1,14 +1,29 @@
 import { MyVehicleRenderer } from './parser/MyVehicleRenderer.js'
 
 class MyVehicle {
+    static selectedVehicle = false
     constructor(file) {
         this.mesh = this._loadVehicle(file)
+        this.selected = false
     }
 
     _loadVehicle(file) {
         const vehicleRenderer = new MyVehicleRenderer()
         const vehicleMesh = vehicleRenderer.render(file)
+        console.log(vehicleMesh)
         return vehicleMesh
+    }
+
+    select() {
+        if (!MyVehicle.selectedVehicle) {
+            this.selected = true
+            MyVehicle.selectedVehicle = true
+        }
+    }
+
+    unselect() {
+        this.selected = false
+        MyVehicle.selectedVehicle = false
     }
 
     controlCar() {
