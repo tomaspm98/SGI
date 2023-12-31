8d88d985f430f732c4ea1037ff3f85fcaaf41d7a
tomaspm98 <up202006845@up.pt>
2023-12-31 18:26:54 +0000

    Start to apply models on powerups and obstacles

--- tp3/circuit/MyPowerUps.js
+++ tp3/circuit/MyPowerUps.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { MyActivatable } from "./MyActivatable.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 class MyPowerUp1 extends MyActivatable {
     constructor(position, rotation, scale, duration) {
@@ -7,12 +8,26 @@ class MyPowerUp1 extends MyActivatable {
         this.effect = "increasedSpeed"
     }
 
+    /*_constructMesh(callback) {
+        const loader = new GLTFLoader();
+        loader.load('./scene/circuits/powerup.glb', (gltf) => {
+            console.log(gltf);
+            const loadedMesh = gltf.scene;
+            callback(loadedMesh); // Call the callback with the loaded mesh
+        },
+        undefined,
+        function (error) {
+            console.error(error);
+        });
+    }*/
+
     _constructMesh() {
         const geometry = new THREE.BoxGeometry(1, 1, 1);
         const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
         const cube = new THREE.Mesh(geometry, material);
         return cube
     }
+
 }
 
 export { MyPowerUp1 };
